9994918e4f21ac57bfc69dc438cdf89bccaa2433
vojtech.vladyka <vojtech.vladyka@gmail.com>
2015-07-26 14:28:03 +0200

    Initial commit. Fork of libopencm3/libopencm3-examples

---
+++ miniblink/miniblink.d
@@ -0,0 +1,16 @@
+miniblink.o: miniblink.c ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/4.8/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/4.8/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f3/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f3/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/f3/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
---
+++ openocd.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+openocd -f /usr/share/openocd/scripts/board/stm32f3discovery.cfg
