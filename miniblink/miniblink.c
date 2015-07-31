386854161b0fdacbaa74a40009f69aa99a3feede
vojtech.vladyka <vojtech.vladyka@gmail.com>
2015-07-31 18:26:54 +0200

    Clean up and commit latest working version

--- miniblink/miniblink.c
+++ miniblink/miniblink.c
@@ -23,15 +23,6 @@
 #include <libopencm3/stm32/gpio.h>
 #include "gpio.h"
 
-// static void gpio_setup(void)
-// {
-// 	/* Enable GPIOE clock. */
-// 	rcc_periph_clock_enable(RCC_GPIOE);
-
-// 	/* Set GPIO12 (in GPIO port E) to 'output push-pull'. */
-// 	gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
-// }
-
 int main(void)
 {
 	int i;
